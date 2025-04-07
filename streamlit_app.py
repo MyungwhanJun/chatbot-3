3fa99c6c7aa2c7ed9e1c1e390f4e74e1f76acfc8
MyungwhanJun <p13097@naver.com>
2025-04-07 13:51:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,10 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 산티아고 순례길 여행 소개 서비스")
 st.write(
     "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "산티아고 순례길 여행을 소개하는 서비스입니다. 궁금한것 질문해 주세요."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
